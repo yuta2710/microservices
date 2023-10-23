ef5f9554e106fbaa4638c41aa97fb0ff402641b8
yuta <127507562+yuta2710@users.noreply.github.com>
2023-10-23 14:06:28 +0700

    Complete Eureka Server section

--- customer/src/main/java/com/yuta/CustomerConfig.java
+++ customer/src/main/java/com/yuta/CustomerConfig.java
@@ -1,5 +1,6 @@
 package com.yuta;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class CustomerConfig {
     @Bean
+    @LoadBalanced // mean that the customer request can come to server 1 or server 2 loading balance
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
